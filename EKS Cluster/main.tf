3fdf08a0b2750053b6b9ab080ff241259078d26a
Murillo Correa <murillocorrea0310@gmail.com>
2025-09-30 22:55:20 -0300

    Adicionando arquivos ao repo

---
+++ EKS Cluster/main.tf
@@ -0,0 +1,60 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "eks-vpc"
+  cidr = var.vpc_cidr
+
+  azs = data.aws_availability_zones.azs.names
+  private_subnets = var.private_subnets
+  public_subnets = var.public_subnets
+  map_public_ip_on_launch = false
+
+  enable_dns_hostnames = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "eks-cluster"
+  kubernetes_version = "1.33"
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+   node = {
+        min_size     = 1
+        max_size     = 3
+        desired_size = 2
+
+        instance_type = ["t3.micro"]     
+    }    
+}
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
